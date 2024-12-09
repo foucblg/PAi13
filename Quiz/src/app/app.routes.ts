2e632f85f9c104b60233df83a36fda07dd791ba2
Daihecyy <paul.saubusse@gmail.com>
2024-12-09 15:51:02 +0100

    Start route

--- Quiz/src/app/app.routes.ts
+++ Quiz/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { QuizComponent } from './quiz/quiz.component';
 
 export const routes: Routes = [
+  { path: 'quiz/start', component: QuizComponent },
   { path: 'quiz/:id', component: QuizComponent },
   { path: '', redirectTo: '/quiz/start', pathMatch: 'full' },
 ];

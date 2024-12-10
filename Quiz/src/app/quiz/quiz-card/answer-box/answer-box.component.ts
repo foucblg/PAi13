c74a9811b963dfac038cc28137744884552f3768
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2024-12-10 17:25:18 +0100

    Working QCM

--- Quiz/src/app/quiz/quiz-card/answer-box/answer-box.component.ts
+++ Quiz/src/app/quiz/quiz-card/answer-box/answer-box.component.ts
@@ -1,12 +1,38 @@
 import { Component, Input } from '@angular/core';
+import { QuizSegment } from '../../../app.component';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-answer-box',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './answer-box.component.html',
   styleUrl: './answer-box.component.css'
 })
 export class AnswerBoxComponent {
-  @Input() possibleAnswers!: string[] | undefined;
-}
+  @Input() quiz_segment: QuizSegment | undefined;
+  @Input() answered : boolean | undefined;
+  qcmForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.qcmForm = this.fb.group({
+      choices: this.fb.array([]),
+    });
+  }
+
+  ngOnInit() {
+    if (this.quiz_segment) {
+      this.addChoices();
+    }
+  }
+
+  get choices(): FormArray {
+    return this.qcmForm.get('choices') as FormArray;
+  }
+
+  private addChoices() {
+    this.quiz_segment?.choices.forEach(() => {
+      this.choices.push(this.fb.control(false)); 
+    });
+  }
+} 

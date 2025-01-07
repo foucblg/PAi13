cf922e644b7cae4dfefce5618cfc8f44f20a001f
Rodkool <rodkool03@gmail.com>
2025-01-07 14:51:09 +0100

    r

--- Jeu/src/app/app.routes.ts
+++ Jeu/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { QuizzComponent } from './quizz/quizz.component';
+import { NavigbuttonComponent } from './quizz/navigbutton/navigbutton.component';
+import { ProgressbarComponent } from './quizz/progressbar/progressbar.component';
+import { ThemeIndicatorComponent } from './quizz/theme-indicator/theme-indicator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'quizz', component: QuizzComponent, children: [
+        { path: '', redirectTo: '0', pathMatch: 'full' },
+    ]
+    },
+];

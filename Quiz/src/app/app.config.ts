a3a9e09f09533f31c8e9004a05544fd6283bdd45
Daihecyy <paul.saubusse@gmail.com>
2025-01-14 17:36:07 +0100

    WOrking primeng

--- Quiz/src/app/app.config.ts
+++ Quiz/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import Material from '@primeng/themes/material';
 
 import { routes } from './app.routes';
 
@@ -10,9 +10,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: Material
       }
     }),
   ]

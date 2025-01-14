923b0b0836d0f0be250ebbd2bce267fc6d076f5b
Azarta2Zygoto <quentin.potiron@gmail.com>
2025-01-14 17:12:44 +0100

    Résolution rapide

--- Jeu/src/app/quizz/navigbutton/navigbutton.component.ts
+++ Jeu/src/app/quizz/navigbutton/navigbutton.component.ts
@@ -19,12 +19,15 @@ export class NavigbuttonComponent {
 
   // Method to increment card number
   ChangeCard() {
-    if (this.avance && this.currentNumber<=11) {
+    if (this.avance && this.currentNumber<10) {
       this.currentNumber += 1;
       this.cat = navigation_data.data[this.currentNumber]?.categorie;
-    } else if (this.currentNumber > 1 ) {
+    } else if (!this.avance && this.currentNumber >= 1 ) {
       this.currentNumber -= 1;
       this.cat = navigation_data.data[this.currentNumber]?.categorie;
+      }
+      else {
+        this.cat = navigation_data.data[this.currentNumber]?.categorie;
       }
 
     this.cardChange.emit(this.currentNumber); // Emit updated card number to parent

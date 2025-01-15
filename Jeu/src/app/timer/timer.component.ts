6f76e4849b1859f548c942c0be0005e2bc7c2110
Daihecyy <paul.saubusse@gmail.com>
2025-01-15 10:24:01 +0100

    Timer full revamp

--- Jeu/src/app/timer/timer.component.ts
+++ Jeu/src/app/timer/timer.component.ts
@@ -1,46 +1,46 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, OnDestroy, OnInit, signal } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css']
+  styleUrls: ['./timer.component.css'],
+  imports: [CommonModule],
+  standalone: true,
 })
-export class TimerComponent implements OnInit, OnDestroy {
-  time: number = 3; // Temps écoulé en secondes
+export class TimerComponent implements OnDestroy {
+  timeInput = input<number>(3); // Temps écoulé en secondes
+  time = signal(this.timeInput());
   private subscription: Subscription = new Subscription();
 
-  ngOnInit(): void {
+  start() {
+    console.log("start")
     // Observable qui émet toutes les secondes
     const timer$ = interval(1000);
 
     // S'abonne pour incrémenter le temps
     this.subscription = timer$.subscribe(() => {
-      this.time--;
+      this.time.update(t => t - 1);
+      if (this.time() <= 0) {
+        this.stop();
+      }
     });
   }
 
   ngOnDestroy(): void {
+    this.stop();
+  }
+  stop(): void {
     // Désabonnement pour éviter les fuites de mémoire
     if (this.subscription) {
+      console.log("stop")
       this.subscription.unsubscribe();
     }
   }
 
   resetTimer(): void {
-    this.time = 0;
-  }
-
-  formatTime(seconds: number): string {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = seconds % 60;
-
-    // Formate chaque partie pour afficher 2 chiffres (ex: 01:09:05)
-    return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(secs)}`;
-  }
-
-  private pad(value: number): string {
-    return value < 10 ? `0${value}` : value.toString();
+    console.log("reset")
+    this.time.set(this.timeInput());
   }
 }

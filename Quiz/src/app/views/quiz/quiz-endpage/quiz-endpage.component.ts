8664d15340288141e44b749e4572698b05d88ee0
Daihecyy <paul.saubusse@gmail.com>
2025-01-29 14:41:03 +0100

    Homepage styling

---
+++ Quiz/src/app/views/quiz/quiz-endpage/quiz-endpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-endpage',
+  imports: [],
+  templateUrl: './quiz-endpage.component.html',
+  styleUrl: './quiz-endpage.component.css'
+})
+export class QuizEndpageComponent {
+
+}

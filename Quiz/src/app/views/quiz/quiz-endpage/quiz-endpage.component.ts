99f112956032a2bf8a05476ef3ffb51b679c9e03
Daihecyy <paul.saubusse@gmail.com>
2025-01-29 19:48:13 +0100

    WIP Endpage styling

--- Quiz/src/app/views/quiz/quiz-endpage/quiz-endpage.component.ts
+++ Quiz/src/app/views/quiz/quiz-endpage/quiz-endpage.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProgressService } from '../../../shared/services/progress-service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-quiz-endpage',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './quiz-endpage.component.html',
   styleUrl: './quiz-endpage.component.css'
 })
 export class QuizEndpageComponent {
-
+  progressService = inject(ProgressService);
 }
